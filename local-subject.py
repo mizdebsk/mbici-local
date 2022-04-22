f56f0a5a0580866494933a2b1c9dd69998f2f42f
Mikolaj Izdebski <mizdebsk@redhat.com>
2022-04-22 15:54:12 +0200

    Initial import

---
+++ local-subject.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+#-
+# Copyright (c) 2021 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Mikolaj Izdebski
+
+import xml.etree.ElementTree as ET
+import argparse
+from subprocess import Popen, PIPE
+
+def resolve_ref(scm, ref):
+    p = Popen(["git", "-C", scm, "rev-parse", ref], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    output, err = p.communicate(b"")
+    if p.returncode != 0:
+        raise Exception(f'git rev-parse failed with exit code {p.returncode}')
+    return output.decode().split()[0]
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-plan", required=True, help="Path to Build Plan")
+parser.add_argument("-scm", default="/home/kojan/tmp/fp", help="SCM base URL")
+parser.add_argument("-ref", default="HEAD", help="Default SCM commit or ref")
+parser.add_argument("-lookaside", default="https://src.fedoraproject.org/lookaside/pkgs/rpms", help="Lookaside cache base URL")
+args = parser.parse_args()
+
+components = set(element.text for element in ET.parse(args.plan).findall('.//component'))
+
+scms = {}
+commits = {}
+
+for component in components:
+    scms[component] = f'{args.scm}/{component}'
+    commits[component] = resolve_ref(scms[component], args.ref)
+
+print(f'<subject>')
+for component in sorted(scms.keys()):
+    print(f'  <component>')
+    print(f'    <name>{component}</name>')
+    print(f'    <scm>{scms[component]}</scm>')
+    print(f'    <commit>{commits[component]}</commit>')
+    print(f'    <lookaside>{args.lookaside}/{component}</lookaside>')
+    print(f'  </component>')
+print(f'</subject>')
---
+++ test-local.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+#-
+# Copyright (c) 2021 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Mikolaj Izdebski
+
+set -eu
+
+plan=/home/kojan/git/mbici-config/plan/bootstrap-maven-rawhide.xml
+platform=/home/kojan/git/mbici-config/platform/rawhide-jdk.xml
+resultDir=/mnt/nfs/mbi-result/local
+cacheDir=/mnt/nfs/mbi-cache
+workDir=/tmp
+PATH=$PATH:/home/kojan/git/mbici-workflow/target
+
+rm -rf test/
+mkdir test
+
+echo === Generating Test Subject from PRs... >&2
+./local-subject.py -plan "$plan" $@ >test/subject.xml
+
+echo === Generating Workflow... >&2
+mbici-wf generate -plan "$plan" \
+     -platform "$platform" \
+     -subject test/subject.xml \
+     -workflow test/workflow.xml \
+     -validate
+
+echo === Generating initial report... >&2
+mbici-wf report \
+     -plan "$plan" \
+     -platform "$platform" \
+     -subject test/subject.xml \
+     -workflow test/workflow.xml \
+     -resultDir "$resultDir" \
+     -reportDir test/report
+
+echo === Running Workflow... >&2
+mbici-wf run \
+     -kubernetesNamespace mbici-local \
+     -maxCheckoutTasks 10 \
+     -maxSrpmTasks 500 \
+     -maxRpmTasks 200 \
+     -maxValidateTasks 20 \
+     -workflow test/workflow.xml \
+     -resultDir "$resultDir" \
+     -cacheDir "$cacheDir" \
+     -workDir "$workDir"
+
+echo === Generating final report... >&2
+rm -rf test/report/
+mbici-wf report \
+     -plan "$plan" \
+     -platform "$platform" \
+     -subject test/subject.xml \
+     -workflow test/workflow.xml \
+     -resultDir "$resultDir" \
+     -reportDir test/report
